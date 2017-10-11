9bae2ba571a197a5726459aa8fd5b91caa032c22
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-10-10 19:46:36 -0700

    Add IBActions for Giants score buttons

--- Scores/ViewController.swift
+++ Scores/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var giantsScorelabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func giantsZero(_ sender: UIButton) {
+    }
+    
+    @IBAction func giantsOne(_ sender: UIButton) {
+    }
+    
+    @IBAction func giantsTwo(_ sender: UIButton) {
+    }
+    
 }
 
